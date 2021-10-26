7f76049ea8c5d5ed257f38f58ee2fd653dd9b049
elmira68 <irandoostelmira@yahoo.com>
2021-10-26 16:21:59 +0300

    Multiplation_Table

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace charp__table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int [,] myarray = new int [10,10];
+          for (int i = 1; i <=10; i++)
+          {
+             for (int j = 1; j <=10; j++)
+             {
+                    // int[] arr = new int[];
+                    int Result = i* j;
+                Console.Write(Result.ToString() +"\t");
+                // Console.Write(myarray[i,j]);
+             } 
+             System.Console.WriteLine();
+          }
+                         
+           
+        }
+    }
+}
